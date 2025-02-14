47cef17dc36fbb6f8bbed69d25b74540c333e87f
DavitBoo <davidmarras@gmail.com>
2025-02-14 18:00:30 +0100

    delete label route

--- src/routes/labelRoutes.ts
+++ src/routes/labelRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createLabel, getAllLabels, getLabelById, updateLabel, deleteLabel } from '../controllers/labelController';
+import passport from 'passport';
 
 const router = express.Router();
 
 router.post('/:postId', createLabel);
 router.get('/', getAllLabels);
-
+router.delete('/:id', passport.authenticate('jwt', { session: false }), deleteLabel);
 
 export default router;
